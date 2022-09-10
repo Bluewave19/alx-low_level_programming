d41a723869a0a43749eef767e83409e37793c3bd
fahad ahmed <fahadahmed3685@gmail.com>
2022-09-09 17:20:47 -0700

    style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  *main - Entry point
@@ -11,14 +9,8 @@
  *
  */ 
 
-
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 		return (0);
-
 }
